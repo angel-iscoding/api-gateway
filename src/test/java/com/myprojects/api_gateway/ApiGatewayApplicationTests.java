d9157d4277b69e23fffe60234673bef86cba22be
angel-iscoding <angel.iscoding@gmail.com>
2024-12-18 22:33:09 -0500

    Initial commit

---
+++ src/test/java/com/myprojects/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
